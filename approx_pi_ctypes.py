b8028c24077e19c6e299b2cbcf10dcbdace5d3d6
Bart Oldeman <bart.oldeman@calculquebec.ca>
2015-10-28 20:39:04 -0400

    Add Ctypes example

---
+++ approx_pi_ctypes.py
@@ -0,0 +1,16 @@
+# approx_pi_ctypes.py
+""" Example of wrapping approx_pi using ctypes. """
+
+import ctypes
+
+# find and load the library
+approx_pi_dll = ctypes.cdll.LoadLibrary('./approx_pi.so')
+# set the argument type
+approx_pi_dll.approx_pi.argtypes = [ctypes.c_int]
+# set the return type
+approx_pi_dll.approx_pi.restype = ctypes.c_double
+
+def approx_pi(arg):
+    ''' Wrapper for approx_pi '''
+    return approx_pi_dll.approx_pi(arg)
+
